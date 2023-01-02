4d8b711193d9f112910c18e6677a8e33154714d2
עמי קרייזל <gnhnjac@gmail.com>
2023-01-02 02:07:54 +0200

    backup'

--- python/Artificial Intelligence/new/test.py
+++ python/Artificial Intelligence/new/test.py
@@ -1,8 +1,23 @@
+def send_message(sender, recipient, message_body, priority=1) -> int:
+   """
+   Send a message to a recipient.
+
+   :param str sender: The person sending the message
+   :param str recipient: The recipient of the message
+   :param str message_body: The body of the message
+   :param priority: The priority of the message, can be a number 1-5
+   :type priority: integer or None
+   :return: the message id
+   :rtype: int
+   :raises ValueError: if the message_body exceeds 160 characters
+   :raises TypeError: if the message_body is not a basestring
+   """
 
 
-import numpy as np
-import skimage.measure
+def foo(sender):
+    """
+    bbb.
 
-a = np.zeros((26, 26))
-print(a.shape)
-print(skimage.measure.block_reduce(a, (2,2), np.max))
+    :param sender: The person sending the message
+    :return: the
+    """
