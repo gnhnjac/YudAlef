0bed352db265202d4f519d7734167209c0e91b17
עמי קרייזל <gnhnjac@gmail.com>
2023-01-19 20:13:00 +0200

    backup

---
+++ osdev/deps/memory.h
@@ -0,0 +1,4 @@
+//refs
+void memcpy (unsigned char* dest , const char* source , int count);
+unsigned char *memset(unsigned char *dest, unsigned char val, int count);
+unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
---
+++ osdev/deps/time.h
@@ -0,0 +1,2 @@
+//refs
+void sleep(int multiplier);
